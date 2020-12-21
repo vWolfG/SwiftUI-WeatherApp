a02acafa08b18de06a8f7c4a2998ebcebc043fa1
Veronika Goreva <veronika.goreva@saritasa.com>
2020-12-21 10:31:25 +0700

    Initialize app

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Veronika Goreva on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
